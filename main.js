1ac5771078a8a8fde2c7503ca9678b8eb2ac72ae
Aashi <aashi2701soni@gmail.com>
2025-09-04 10:37:07 +0530

    added quotation animation and removed swiper js

--- main.js
+++ main.js
@@ -54,25 +54,3 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
-// swiper js
-// swiper js
-// swiper js
-
-var swiper = new Swiper(".slide-content", {
-  slidesPerView:3,
-  spaceBetween:30,
-  slidesPerGroup:3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-
-  },
-  navigation: {
-    nextE1: ".swiper-button-next",
-    prevE1: ".swiper-button-prev",
-  },
-});
-
